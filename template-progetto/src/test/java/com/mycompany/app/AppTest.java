2734d9632240c80b510e2c5a9f642707f01c2440
RenatoImparat0 <giuseppe.imparato@itsrizzoli.it>
2025-11-21 17:29:38 +0100

    Aggiunta test

--- template-progetto/src/test/java/com/mycompany/app/AppTest.java
+++ template-progetto/src/test/java/com/mycompany/app/AppTest.java
@@ -1,6 +1,7 @@
 package com.mycompany.app;
 
 import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.assertEquals;
 
 import org.junit.Test;
 
@@ -9,6 +10,13 @@ public class AppTest
     @Test
     public void shouldAnswerWithTrue()
     {
-        assertTrue( false );
+        assertTrue( true );
+    }
+
+    @Test
+    public void testInventario() {
+        NegozioMusica nm = new NegozioMusica();
+        Pianoforte piano1 = new Pianoforte("Yamaha", "S5X", "Nero lucido", 2013, 6090005);
+        assertEquals("Yamaha", piano1.getMarca());
     }
 }
